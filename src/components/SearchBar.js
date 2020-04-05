e6d5bc2bae9382727d994cc78a5ce35ea3d02653
Jenny Tran <jennny.tran@gmail.com>
2020-04-05 16:10:11 +0300

    Set tab width to 2

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,40 +3,40 @@ import '../css/Form.css';
 import FontAwesome from 'react-fontawesome';
 
 class SearchBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            loading: false
-        };
-    }
-
-    handleChange = event => {
-        this.setState({ value: event.target.value });
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      loading: false
     };
+  }
 
-    sendValueToParent = event => {
-        event.preventDefault();
-        this.props.callbackFromParent(this.state.value);
-    };
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
 
-    render() {
-        return (
-            <form id="pokemon-search" onSubmit={this.sendValueToParent}>
-                <label>
-                    Name or ID:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                {this.props.loading ? (
-                    <FontAwesome name="spinner" spin />
-                ) : (
-                    <button type="submit" value="Search">
-                        <FontAwesome name="search" />{' '}
-                    </button>
-                )}
-            </form>
-        );
-    }
+  sendValueToParent = event => {
+    event.preventDefault();
+    this.props.callbackFromParent(this.state.value);
+  };
+
+  render() {
+    return (
+      <form id="pokemon-search" onSubmit={this.sendValueToParent}>
+        <label>
+          Name or ID:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        {this.props.loading ? (
+          <FontAwesome name="spinner" spin />
+        ) : (
+          <button type="submit" value="Search">
+            <FontAwesome name="search" />{' '}
+          </button>
+        )}
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
--- src/components/TypeFilters.js
+++ src/components/TypeFilters.js
@@ -3,76 +3,76 @@ import Type from './Type.js';
 import '../css/Typefilters.css';
 
 class TypeFilters extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            types: null,
-            filteredPokemons: null,
-            gotDataFromFilterSearch: false,
-            hideResults: false
-        };
-    }
-
-    pokemonsFromFilterSearch = dataFromFilter => {
-        this.setState({ filteredPokemons: dataFromFilter, gotDataFromFilterSearch: true });
+  constructor(props) {
+    super(props);
+    this.state = {
+      types: null,
+      filteredPokemons: null,
+      gotDataFromFilterSearch: false,
+      hideResults: false
     };
+  }
 
-    getTypes = () => {
-        const url = 'https://pokeapi.co/api/v2/type/';
-        const request = new Request(url);
-        fetch(request).then(response => {
-            response.json().then(data => {
-                const types = data.results
-                    .map(typeObj => typeObj.name)
-                    // splice last two empty types
-                    .splice(0, data.results.length - 2);
-                this.setState({ types });
-            });
-        });
-    };
+  pokemonsFromFilterSearch = dataFromFilter => {
+    this.setState({ filteredPokemons: dataFromFilter, gotDataFromFilterSearch: true });
+  };
 
-    searchPokemon = event => {
-        this.props.callbackFromParent(event.target.id);
-        this.setState({ hideResults: true });
-    };
+  getTypes = () => {
+    const url = 'https://pokeapi.co/api/v2/type/';
+    const request = new Request(url);
+    fetch(request).then(response => {
+      response.json().then(data => {
+        const types = data.results
+          .map(typeObj => typeObj.name)
+          // splice last two empty types
+          .splice(0, data.results.length - 2);
+        this.setState({ types });
+      });
+    });
+  };
 
-    componentDidMount() {
-        this.getTypes();
-    }
+  searchPokemon = event => {
+    this.props.callbackFromParent(event.target.id);
+    this.setState({ hideResults: true });
+  };
 
-    render() {
-        const { types, gotDataFromFilterSearch, filteredPokemons, hideResults } = this.state;
+  componentDidMount() {
+    this.getTypes();
+  }
 
-        const typeElems =
-            types &&
-            types.map((type, i) => (
-                <Type
-                    key={`type${i}-${type}`}
-                    type={type}
-                    callbackFromParent={this.pokemonsFromFilterSearch}
-                />
-            ));
+  render() {
+    const { types, gotDataFromFilterSearch, filteredPokemons, hideResults } = this.state;
 
-        const pokemonsFromFilter =
-            gotDataFromFilterSearch &&
-            filteredPokemons.map(pokemon => (
-                <button
-                    onClick={this.searchPokemon}
-                    key={pokemon}
-                    id={pokemon}
-                    className="TypeFilter-pokeBtn"
-                >
-                    {pokemon}
-                </button>
-            ));
+    const typeElems =
+      types &&
+      types.map((type, i) => (
+        <Type
+          key={`type${i}-${type}`}
+          type={type}
+          callbackFromParent={this.pokemonsFromFilterSearch}
+        />
+      ));
 
-        return (
-            <div className="TypeFilters">
-                <div className="TypeFilters-container">{typeElems}</div>
-                <div className="TypeFilters-results">{!hideResults && pokemonsFromFilter}</div>
-            </div>
-        );
-    }
+    const pokemonsFromFilter =
+      gotDataFromFilterSearch &&
+      filteredPokemons.map(pokemon => (
+        <button
+          onClick={this.searchPokemon}
+          key={pokemon}
+          id={pokemon}
+          className="TypeFilter-pokeBtn"
+        >
+          {pokemon}
+        </button>
+      ));
+
+    return (
+      <div className="TypeFilters">
+        <div className="TypeFilters-container">{typeElems}</div>
+        <div className="TypeFilters-results">{!hideResults && pokemonsFromFilter}</div>
+      </div>
+    );
+  }
 }
 
 export default TypeFilters;
